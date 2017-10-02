f2940e2bd01cd08bda697b137a10f05beb5c3d2f
Ramanand Shankarling <ramanandsg@gmail.com>
2017-10-02 00:09:27 -0400

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,5 +1,8 @@
 module.exports = {
 
 	// the database url to connect!
-	url: 'mongodb://todoAholic:scotchio@ec2-52-0-14-185.compute-1.amazonaws.com:27017/dummyDB'
+	//url: 'mongodb://todoAholic:scotchio@ec2-52-0-14-185.compute-1.amazonaws.com:27017/dummyDB'
+	
+	//my custom database url
+	url: 'mongodb://newram:welcome123@ec2-34-229-63-0.compute-1.amazonaws.com:27017/dummyDB'
 }
